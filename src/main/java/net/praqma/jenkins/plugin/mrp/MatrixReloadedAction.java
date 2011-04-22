55cc81b528a53a2332e5ecf8c8e3320bf9ae932a
Christian Wolfgang <wolfgarnet@gmail.com>
2011-04-22 23:26:21 +0200

    Added a lot of features + files

--- src/main/java/net/praqma/jenkins/plugin/mrp/MatrixReloadedAction.java
+++ src/main/java/net/praqma/jenkins/plugin/mrp/MatrixReloadedAction.java
@@ -1,16 +1,33 @@
 package net.praqma.jenkins.plugin.mrp;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import javax.servlet.ServletException;
+
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
 
 import org.kohsuke.stapler.StaplerRequest;
 import org.kohsuke.stapler.StaplerResponse;
 
+import hudson.model.AbstractBuild;
+import hudson.model.BooleanParameterValue;
 import hudson.model.Action;
+import hudson.model.Cause;
+import hudson.model.CauseAction;
+import hudson.model.Hudson;
+import hudson.model.ParameterDefinition;
+import hudson.model.ParameterValue;
+import hudson.model.ParametersAction;
+import hudson.model.ParametersDefinitionProperty;
+import hudson.model.StringParameterValue;
 
 public class MatrixReloadedAction implements Action
 {
+	private AbstractBuild<?, ?> build;
 
 	public String getDisplayName()
 	{
@@ -19,7 +36,8 @@ public class MatrixReloadedAction implements Action
 
 	public String getIconFileName()
 	{
-		return "";
+		//return "matrix_small.png";
+		return "/plugin/mrp/images/matrix_small.png";
 	}
 
 	public String getUrlName()
@@ -27,9 +45,89 @@ public class MatrixReloadedAction implements Action
 		return "matrix-reloaded";
 	}
 	
+    public AbstractBuild<?, ?> getBuild() {
+        return build;
+    }
+    
+    public String getString()
+    {
+    	return "MY OWN STRING";
+    }
+	
 	public void doConfigSubmit( StaplerRequest req, StaplerResponse rsp ) throws ServletException, IOException, InterruptedException
 	{
+		AbstractBuild<?, ?> build = req.findAncestorObject(AbstractBuild.class);
 		
+		List<ParameterValue> values = new ArrayList<ParameterValue>();
+		
+        JSONObject formData = req.getSubmittedForm();
+        Iterator<?> it = formData.keys();
+        
+        /* Generate the parameters */
+        while( it.hasNext() )
+        {
+        	String key = (String)it.next();
+        	System.out.println( "[WOLLE] key=" + key ); 
+        	/* Check the field */
+        	if( key.startsWith( "mrp::" ) )
+        	{
+        		String[] vs = key.split( "::", 2 );
+        		
+        		String value = formData.getString( key );
+        		boolean checked = formData.getBoolean( key );
+        		
+        		System.out.println( "[WOLLE] key=" + key + ", value=" + value );
+        		
+        		boolean reuse = false;
+        		
+        		/**/
+        		if( vs.length > 1 && checked )
+        		{
+        			System.out.println( "[WOLLE] YAY" );
+        			reuse = true;
+        		}
+        		
+        		/* Create the parameter */
+        		if( vs.length > 1 )
+        		{
+        			System.out.println( "[WOLLE] creating parameter" );
+        			values.add( new BooleanParameterValue( key, reuse ) );
+        		}
+        	}
+        	
+        	if( key.equals( "MRP::NUMBER" ) )
+        	{
+        		String value = formData.getString( key );
+        		values.add( new StringParameterValue( "MRP_NUMBER", value ) );
+        	}
+        }
+        
+        //System.out.println( "[WOLLE] OWNER=" + paramDefprop.getOwner().getDisplayName() );
+        System.out.println( "[WOLLE] OWNER=" + build.getProject().getDisplayName() );
+        System.out.println( "[WOLLE] SIZE OF: " + values.size() );
+        
+        Hudson.getInstance().getQueue().schedule( 
+        		build.getProject(), 0, new ParametersAction(values), new CauseAction(new Cause.UserCause())
+        );
+        
+        
+        
+		rsp.sendRedirect( "../../" );
+        
+        /*
+        JSONArray a = JSONArray.fromObject(formData.get("parameter"));
+        
+        for (Object o : a) {
+            JSONObject jo = (JSONObject) o;
+            String name = jo.getString("name");
+            ParameterDefinition d = paramDefprop.getParameterDefinition(name);
+            if (d == null) {
+                throw new IllegalArgumentException("No such parameter definition: " + name);
+            }
+            ParameterValue parameterValue = d.createValue(req, jo);
+            System.out.println( "[WOLLE] Parameter=" + name + "," + d.getName() );
+        }
+        */
 	}
 
 }
--- src/main/java/net/praqma/jenkins/plugin/mrp/MatrixReloadedListener.java
+++ src/main/java/net/praqma/jenkins/plugin/mrp/MatrixReloadedListener.java
@@ -1,10 +1,22 @@
 package net.praqma.jenkins.plugin.mrp;
 
+import java.io.IOException;
+import java.util.List;
+
+import hudson.EnvVars;
+import hudson.Extension;
+import hudson.matrix.MatrixRun;
+import hudson.matrix.MatrixBuild;
 import hudson.model.AbstractBuild;
+import hudson.model.BooleanParameterValue;
+import hudson.model.ParameterValue;
 import hudson.model.TaskListener;
+import hudson.model.ParametersAction;
 import hudson.model.Run;
+import hudson.model.StringParameterValue;
 import hudson.model.listeners.RunListener;
 
+@Extension
 public class MatrixReloadedListener extends RunListener<Run>
 {
 	public MatrixReloadedListener()
@@ -12,11 +24,125 @@ public class MatrixReloadedListener extends RunListener<Run>
 		super( Run.class );
 	}
 	
-	public void onStarted( Run run, TaskListener listener)
+	/**
+	 * 
+	 */
+	@Override
+	public void onStarted( Run run, TaskListener listener )
 	{
-		if( run instanceof AbstractBuild )
+		//System.out.println( "[WOLLE] Started.... " + run.getDisplayName() );
+		System.out.println( "[WOLLE] Started.... " + run.getParent().getDisplayName() );
+		
+		/* Parse ParametersAction */
+        /* First try to find the correct parameter for the project */
+		/* Test for MatrixBuild */
+		if( run instanceof MatrixRun )
 		{
+			listener.getLogger().println( "[MRP] This is a matrix run" );
+			
+	        try
+	        {
+	        	
+	        	List<ParametersAction> pvs2 = run.getActions( ParametersAction.class );
+	        	System.out.println( "[WOLLE] SIZE OF PARMS2: " + pvs2.size() );
+	        	
+	            List<ParameterValue> pvs = run.getActions( ParametersAction.class ).get(0).getParameters();
+	            System.out.println( "[WOLLE] SIZE OF PARMS: " + pvs.size() );
+	            MatrixRun mr = (MatrixRun)run;
+	            
+	            StringParameterValue mrpNumber = (StringParameterValue)getParameterValue( pvs, "MRP_NUMBER" );
+	            System.out.println( "[WOLLE] mrpNumnber=" + mrpNumber );
+	            int mnumber = Integer.parseInt( mrpNumber.value );
+	            	            
+	            System.out.println( "[WOLLE] NAME: " + mr.getParent().getCombination().toString() );
+	        	for( ParameterValue pv : pvs )
+	        	{
+	        		listener.getLogger().println( "[MRP] * " + pv.getName() );
+	        		
+	        		if( pv.getName().startsWith( "mrp::" ) )
+	        		{
+	        			String[] vs = pv.getName().split( "::", 2 );
+	        			if( vs[1].equals( mr.getParent().getCombination().toString() ) )
+	        			{
+	        				listener.getLogger().println( "[MRP] There was a match! " );
+	        				BooleanParameterValue pvb = (BooleanParameterValue)pv;
+	        				
+	        				/* Reusing */
+	        				if( pvb.value )
+	        				{
+	        					run.setReuse( mnumber );
+	        				}
+	        				
+	        				/*
+	        				try
+							{
+								EnvVars ev = run.getEnvironment( listener );
+								ev.put( "MRP_NUMBER", mrpNumber.value );
+								listener.getLogger().println( "[MRP] Created MRP_NUMBER" );
+							}
+							catch( Exception e )
+							{
+								listener.getLogger().println( "[MRP] Unable to create MRP_NUMBER" );
+							}
+							*/
+							
+							/* We could break here... */
+							break;
+	        			}
+	        			
+	        		}
+	        	}
+	        }
+	        catch( IndexOutOfBoundsException e )
+	        {
+	        	listener.getLogger().println( "[MRP] No parameters" );
+	        }
+		}
+
+	}
+	
+	private ParameterValue getParameterValue( List<ParameterValue> pvs, String key )
+	{
+    	for( ParameterValue pv : pvs )
+    	{
+    		if( pv.getName().equals( key ) )
+    		{
+    			System.out.println( "[WOLLE] I GOT IT!!" );
+    			return pv;
+    		}
+    	}
+    	
+    	System.out.println( "[WOLLE] I DIDNT GET IT!!" );
+    	return null;
+	}
+	
+	/**
+	 * Add the Matrix Reloaded link to the build context
+	 */
+	@Override
+	public void onCompleted( Run run, TaskListener listener )
+	{
+		System.out.println( "[WOLLE] Completed...." );
+		
+		/* Test for MatrixBuild */
+		if( run instanceof MatrixBuild )
+		{
+			System.out.println( "[WOLLE] Yes, this is a matrix build! " + run.getParent().getDisplayName() + " " + run.getDisplayName() );
+			
 			AbstractBuild build = (AbstractBuild)run;
+
+			MatrixReloadedAction action = new MatrixReloadedAction();
+			build.getActions().add( action );
+		}
+		else
+		{
+			System.out.println( "[WOLLE] This was NOT a matrix build!" );
+		}
+		
+		/* Test for MatrixRun */
+		if( run instanceof MatrixRun )
+		{
+			
 		}
 	}
 
